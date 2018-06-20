021bfc45f2f1edb2dc26651ea281c85006be24d7
Dhaval Chheda <chhedadhaval120386@gmail.com>
2018-06-20 22:49:14 +0530

    Second version of the app

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          This is version 1.0.0
+          This is version 2.0.0
         </p>
       </div>
     );
